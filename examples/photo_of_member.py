29777c8e5945c49c5b91e098f696c7135e93eec8
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2022-08-17 17:32:51 +0200

    Fix linting

--- examples/photo_of_member.py
+++ examples/photo_of_member.py
@@ -6,7 +6,7 @@ import requests
 client = goifer.Client("canton_zurich")
 member = client.search(
     index="Mitglieder",
-    query='Wahlkreis adj "IX Horgen" and dauer_start > "2019-11-19 00:00:00"'
+    query='Wahlkreis adj "IX Horgen" and dauer_start > "2019-11-19 00:00:00"',
 )
 
 member = member[0]
@@ -14,13 +14,13 @@ pprint(member)
 
 # download image
 
-foto_id = member['foto_id']
-version = member['foto_version']['nr']
-view = member['foto_version']['rendition'][0]['ansicht']
-ext = member['foto_version']['rendition'][0]['extension']
+foto_id = member["foto_id"]
+version = member["foto_version"]["nr"]
+view = member["foto_version"]["rendition"][0]["ansicht"]
+ext = member["foto_version"]["rendition"][0]["extension"]
 
 download_path = os.path.join(".", f"foto.{ext}")
-download_url = client.file("Mitglieder", foto_id, version, view) 
+download_url = client.file("Mitglieder", foto_id, version, view)
 
 r = requests.get(download_url)
 print(f"Download file {download_url}...")
@@ -29,7 +29,8 @@ with open(download_path, "wb") as f:
         f.write(chunk)
 
 try:
-    from PIL import Image  
+    from PIL import Image
+
     img = Image.open(download_path)
     img.show()
 except ImportError:
